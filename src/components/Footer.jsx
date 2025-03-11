0bb444f7a03450a82e282876ae8e874fe89a9e10
DevSuhail <suhail6487@gmail.com>
2025-03-11 16:07:43 +0530

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-indigo-950 text-white py-2 mt-10">
       <div className="container mx-auto text-center">
         <p className="text-sm">
-          &copy; {new Date().getFullYear()} Movie Finder. All rights reserved.
+          &copy; {new Date().getFullYear()} cinexplore. All rights reserved.
         </p>
         <p className="text-sm">
           Made with ❤️ by <a href="https://github.com/DevSuhail" target="_blank" className="text-purple-400 hover:text-purple-300">DevSuhail</a>
